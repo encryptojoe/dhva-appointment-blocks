2f1d3e2c37d26338ac50e30c116f40a6a0399a51
colin-mccann <mccann.colinp@gmail.com>
2016-10-02 11:44:00 -0400

    Updating audit

--- audit.php
+++ audit.php
@@ -1,15 +1,19 @@
 <?php
-if (array_key_exists('patid', $_POST)) {
-	header('Location: doAudit.php?id=' . $_POST['patid']);
-	die();
+
+if (!array_key_exists('run', $_POST)) {
+?>
+<h2>Run Audit</h2>
+<form method='POST'>
+	<input type="hidden" name="run" value="1">
+	<input type="submit" value="Run Audit">
+</form>
+<?php 
+	exit();
 }
-
-
-##################
+#####################################
 
 
 include "tierion.php";
-
 $iniConfig = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/../conf/dhva.ini', true);
 $db        = new mysqli($iniConfig['db_va']['db_server'], $iniConfig['db_va']['db_username'], $iniConfig['db_va']['db_password'], $iniConfig['db_va']['db_database']);
 
@@ -20,10 +24,9 @@ if ($db->connect_errno > 0) {
     exit;
 }
 
-
-function getPatients($db, $lname) {
+function getApptInfo($db) {
 	$a = [];
-	$sql = "SELECT * FROM PATIENT WHERE UPPER(FAMILY) LIKE '%" . strtoupper($lname) . "%' ORDER BY FAMILY";
+	$sql = 'SELECT * FROM APPOINTMENT ORDER BY ID';
 	if ($all = $db->query($sql)) {
 		while ($row = $all->fetch_assoc()) {
 			$a[] = $row;
@@ -33,25 +36,91 @@ function getPatients($db, $lname) {
 	return $a;
 }
 
-?>
-<h2>Audit Page</h2>
-<h4>Please type a patient last name, then select the correct patient<h4>
-<form method='POST'>
-	<input type="text" name="last">
-
-<?php 
-if (array_key_exists('last', $_POST)) {
-	echo "<br />";
-	echo "<select name='patid'>";
-	$pats = getPatients($db, $_POST['last']);
-	foreach ($pats as $pat) {
-		echo '<option value="' . $pat['ID'] . '">' . $pat['FAMILY'] . ', ' . $pat['NAME'] . '</option>';
+function getPatient($db, $patId) {
+	$a = null;
+	$sql = "SELECT * FROM PATIENT WHERE ID=" . $patId . " ORDER BY FAMILY";
+	if ($all = $db->query($sql)) {
+		while ($row = $all->fetch_assoc()) {
+			$a = $row;
+		}
+		$all->free();
 	}
-	echo "</select>";
-	echo "<br />";
+	return $a;
 }
-?>
 
-	<input type="submit" value="Submit">
-</form>
+
+function getBCApptInfo($id)
+{
+    $recs = getAllRecords();
+    $recsjson = json_decode($recs, True);
+    $recsjson = $recsjson['records'];
+
+    $out_arr = null; 
+    foreach ($recsjson as $rec) {
+        $url = 'https://api.tierion.com/v1/records/' . $rec['id'];
+        $recordresponse = doCurl($url);
+
+        $res = json_decode($recordresponse, True);
+        $res = $res['data'];
+    
+
+        if (array_key_exists('version', $res) && $res['version'] == '0.1') {
+                if ($res['appointment'] == $id) {
+			$out_arr = $res;
+			break;
+                }
+        }
+    }
+    return $out_arr;
+}
+
+$apptInfos = getApptInfo($db);
+foreach ($apptInfos as $apptInfo) {
+	$pat = getPatient($db, $apptInfo['PATIENT_ID']);
+	$apptInfo['NAME'] = $pat['FAMILY'] . ' ' . $pat['NAME'];
+	$apptInfo['BC'] = getBCApptInfo($apptInfo);
+
+	$toHash = $apptInfo['PATIENT_ID'] . 'na'. $apptInfo['ID'] . $apptInfo['STAFF_ID'];
+	$apptInfo['CalcHash'] = getHash($toHash);
+	$apptInfo['HashMatch'] = True;
+	if ($apptInfo['CalcHash'] != $apptInfo['HASH']) {
+		$apptInfo['HashMatch'] = False;
+	}
+}
+
+
+function writeTableRow($rowName, $mySQLVal, $bcVal) 
+{
+	if ($mySQLVal != $bcVal) {
+		echo '<tr bgcolor="pink">';
+	} else {
+		echo '<tr>';
+	}
+	echo '<td>' . $rowName . '</td>';
+	echo '<td>' . $mySQLVal. '</td>';
+	echo '<td>' . $bcVal . '</td></tr>';
+}
+
+#############################
+echo "<h2>Audit Results</h2>";
+foreach ($apptInfos as $apptInfo) {
+	echo '<h3>Appointment for: ' . $apptInfo['NAME'] . ' on ' . $apptInfo['L_TS'] . '</h3>';
+	echo "<table style='width:50%'>";
+
+	echo '<th>Value</th><th>Local DB</th><th>Blockchain</th>';
+		
+	writeTableRow('Timestamp', $apptInfo['L_TS'], $apptInfo['BC']['timestamp']);
+	writeTableRow('Attending ID', $apptInfo['STAFF_ID'],  $apptInfo['BC']['attending']);
+	writeTableRow('Row Hash', $apptInfo['HASH'],  $apptInfo['BC']['rowhash']);
+	echo '<tr><td>Hash Matches Data?</td>';
+	if ($apptInfo['HashMatch']) {
+		echo "<td>YES</td><td></td></tr>";
+	} else {
+		echo "<td bgcolor='pink'>NO, data changed</td><td></td></tr>";
+	}
+	echo '</table>';
+	echo '<br />';
+}
+
+?>
 
