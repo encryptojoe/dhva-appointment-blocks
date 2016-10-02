410464713012a3c2f66eaac126cbded16f0af1e4
Justin <justinmcnm@gmail.com>
2016-10-02 10:02:54 -0500

    Working save confirmation screne. Need to add the api.php changes. So I am commiting what I have

---
+++ js/components/ApptSaved.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Row, Col, Alert} from 'react-bootstrap';
+
+import React from 'react';
+
+export const ApptSaved = React.createClass({
+    render() {
+        return (
+            <div></div>
+        );
+    }
+});
--- js/index.js
+++ js/index.js
@@ -4,15 +4,16 @@ import { IndexRoute, Router, Route, IndexRedirect, hashHistory } from 'react-rou
 
 import App from './components/App';
 
-// import Calendar from './components/Calendar';
 import CreateAppt from './components/CreateAppt';
 import ListAppts from './components/ListAppts';
+import ApptSaved from './components/ApptSaved';
 
 const router = (
     <Router history={hashHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={CreateAppt}/>
             <Route path="/appointment-list" component={ListAppts} />
+            <Route path="/appointment-saved" component={ApptSaved} />
         </Route>
     </Router>
 );
