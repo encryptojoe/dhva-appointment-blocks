92fe0c603e4e7acb75c71ecae76fa38f99736b54
Justin <justinmcnm@gmail.com>
2016-10-02 09:57:39 -0500

    Added waitlist button. Made it only show if user selects no avail time.

--- js/components/CreateAppt.js
+++ js/components/CreateAppt.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import moment from 'moment';
 import {Panel, Row, Col, Form, FormGroup, FormControl, ControlLabel, ButtonToolbar, Button} from 'react-bootstrap';
-import DatePicker from 'react-datepicker';
+// import DatePicker from 'react-datepicker';
 import Calendar from './Calendar';
 import DoctorDrop from './DoctorDrop';
 import R from 'ramda';
+
+import DateTimePick from './DateTimePick';
 
 const CreateAppt = React.createClass({
     saveReady:function(){
@@ -14,68 +16,12 @@ const CreateAppt = React.createClass({
            cstate.appointment.time != '00:00:00'
         ){return true;} else {return false;}
     },
-    dayTimePickShow:function(){
-        let cstate = this.props.rootState;
-        if(cstate.appointment.department > -1){
-            return true;
-        } else {return false;}
-    },
-    genTimeSlots:function(){
-        function addMinutes(date, minutes) {
-            return new Date(date.getTime() + minutes*60000);
-        }
-        let d = new Date();
-        let result = new Array();
-        for (let idx = 0; idx < 10; idx++) {
-            let m = (((d.getMinutes() + 7.5)/15 | 0) * 15) % 60;
-            let h = ((((d.getMinutes()/105) + .5) | 0) + d.getHours()) % 24;
-            d = new Date(d.getYear(), d.getMonth(), d.getDay(), h, m, 0, 0);
-
-            // if (idx > 0) result += ", ";
-            let ctime = ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2);
-            result.push(ctime);
-            
-            d = addMinutes(d, 15);
-        }
-        return result;
-    },
+    
+    
     render:function(){
-        const slots        = this.genTimeSlots();
-        const cbs          = {marginTop:10};
-        // const btns         = <FormGroup controlId="buttons"><ButtonToolbar>  {(this.saveReady()) ? <Button bsStyle ="primary">Create</Button> : <span></span>} </ButtonToolbar> </FormGroup>;
-        const dateTimePick = (this.dayTimePickShow()) ? 
-            <div>
-                <FormGroup controlId="date-pick">
-                    <Row>
-                        <Col sm={5}>
-                            <ControlLabel>Pick Day</ControlLabel>
-                        </Col>
-                        <Col sm={5}>
-                            <ControlLabel>Pick Time</ControlLabel>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={5}>
-                            <DatePicker inline selected={this.props.rootState.appointment.date} minDate={moment()} onChange={this.props.updateApptDate} />
-                        </Col>
-                        <Col sm={5}>
-                            <FormControl componentClass="select" placeholder="select" onChange={this.props.updateTime}>
-                                <option value="00:00">Select</option>
-                                {
-                                    slots.map(function(time, i){
-                                        return <option key={time}>{time}</option>
-                                    })
-                                }
-                            </FormControl>
-                            {(this.props.rootState.appointment.time != '00:00') ? <Button style={cbs} bsStyle="primary">Create</Button> : <div></div>}
-                        </Col>
-                    </Row>
-
-                </FormGroup>
-            </div>
-             : <div></div>;
-
-            const CalAvail = (this.props.rootState.appointment.department == -1) ? <FormGroup controlId="calendar"></FormGroup>: <FormGroup controlId="calendar">
+        
+        
+        const CalAvail     = (this.props.rootState.appointment.department == -1) ? <FormGroup controlId="calendar"></FormGroup>: <FormGroup controlId="calendar">
                             <Calendar
                                 onNextMonth={() => this.props.updateDate(this.props.rootState.date.clone().add(1, 'months') ) }
                                 onPrevMonth={() => this.props.updateDate(this.props.rootState.date.clone().subtract(1, 'months') ) }
@@ -88,13 +34,13 @@ const CreateAppt = React.createClass({
         return (
             <Panel bsStyle="primary" header="Create New Appointment">
                 <Col sm={12}>
-                    <h3>Patient: Joanna</h3>
+                    <h2>Patient: Joanna</h2>
                     <Form horizontal>
 
                         <FormGroup controlId="department">
                             <ControlLabel>Department</ControlLabel>
                             <FormControl componentClass="select" placeholder="select" onChange={this.props.update_patient_department}>
-                                <option value="select">Select</option>
+                                <option value="-1">Select</option>
                                 {
                                     this.props.rootState.departments.map(function(dept, i){
                                         return <option key={dept.ID} value={dept.ID}>{dept.DEPARTMENT}</option>
@@ -105,7 +51,7 @@ const CreateAppt = React.createClass({
 
                         <DoctorDrop {...this.props} />
 
-                        {dateTimePick}
+                        <DateTimePick {...this.props}/>
 
                         {CalAvail}
                     </Form>
---
+++ js/components/DateTimePick.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { FormGroup, Row, Col, ControlLabel, FormControl, Button} from 'react-bootstrap';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+
+const DateTimePick = React.createClass({
+    dayTimePickShow:function(){
+        let cstate = this.props.rootState;
+        if(cstate.appointment.department > -1){
+            return true;
+        } else {return false;}
+    },
+    genTimeSlots:function(){
+        function addMinutes(date, minutes) {
+            return new Date(date.getTime() + minutes*60000);
+        }
+        let d = new Date();
+        let result = new Array();
+        for (let idx = 0; idx < 10; idx++) {
+            let m = (((d.getMinutes() + 7.5)/15 | 0) * 15) % 60;
+            let h = ((((d.getMinutes()/105) + .5) | 0) + d.getHours()) % 24;
+            d     = new Date(d.getYear(), d.getMonth(), d.getDay(), h, m, 0, 0);
+
+            let ctime = ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2);
+            result.push(ctime);
+            
+            d = addMinutes(d, 15);
+        }
+        return result;
+    },
+    render() {
+        if(!this.dayTimePickShow()){return <div></div>;}
+
+        const slots = this.genTimeSlots();
+        const cbs   = {marginTop:10};
+
+        if(this.props.rootState.appointment.time != '00:00' && this.props.rootState.appointment.time != 'waitlist'){
+            var btns = <Button style={cbs} bsStyle="primary">Create</Button>;
+        } else if(this.props.rootState.appointment.time == 'waitlist'){
+            var btns = <Button bsStyle="danger" style={cbs}>Add to Waitlist</Button>;
+        } else {
+            var btns = <div></div>;
+        }
+
+        return (
+            <div>
+                <FormGroup controlId="date-pick">
+                    <Row>
+                        <Col sm={5}>
+                            <ControlLabel>Pick Day</ControlLabel>
+                        </Col>
+                        <Col sm={5}>
+                            <ControlLabel>Pick Time</ControlLabel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={5}>
+                            <DatePicker inline selected={this.props.rootState.appointment.date} minDate={moment()} onChange={this.props.updateApptDate} />
+                        </Col>
+                        <Col sm={5}>
+                            <FormControl componentClass="select" placeholder="select" onChange={this.props.updateTime}>
+                                <option value="00:00"></option>
+                                {
+                                    slots.map(function(time, i){
+                                        return <option key={time}>{time}</option>
+                                    })
+                                }
+                                <option value="waitlist">No Avail Times that work</option>                                
+                            </FormControl>
+                            {btns}
+                        </Col>
+                    </Row>
+                </FormGroup>
+            </div>
+        );
+    }
+});
+
+export default DateTimePick;
